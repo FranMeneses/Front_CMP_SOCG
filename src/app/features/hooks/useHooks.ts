309695153a23131172b97e4c2dd4c1ab88c58749
CCliftS <90357585+CCliftS@users.noreply.github.com>
2025-05-20 09:51:24 -0400

    ADD: Reportability information

--- src/app/features/hooks/useHooks.ts
+++ src/app/features/hooks/useHooks.ts
@@ -5,7 +5,7 @@ import { IValley } from "@/app/models/IValleys";
 
 export function useHooks() {
     const router = useRouter();
-    const [userRole, setUserRole] = useState<string>("encargado asuntos públicos");	
+    const [userRole, setUserRole] = useState<string>("encargado cumplimiento");	
     const [currentValley, setCurrentValley] = useState<IValley | null>(null);
     const { valleys, faenas } = useData();
 
